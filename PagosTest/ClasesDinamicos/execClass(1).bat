ba4ebf8a3a43bace6d7c1deee8ecfb430adbab89
Cecijvp <cecijvp.3@gmail.com>
2019-12-16 15:31:17 -0600

    correción de archivos repetidos

--- PagosTest/ClasesDinamicos/execClass(1).bat
+++
@@ -1,4 +0,0 @@
-set path=C:\Program Files\Java\jdk1.8.0_221\bin;%path%
-set classpath=.;C:\Program Files\Java\jdk1.8.0_221;classpath%
-
-java clasesdinamicos.Distribuidor clasesdinamicos.HolaMundo uno dos tres mill
